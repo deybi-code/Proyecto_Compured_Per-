6d46bbd093303786e2353a5d3efd5ad43da1e027
deybi-code <deybigavidia0@gmail.com>
2026-06-25 17:52:33 -0500

    Fix: Encriptación de contraseña para inicio de sesión

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,23 +3,24 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // Esto creará al administrador con la contraseña encriptada correctamente
+        User::create([
+            'nombre_completo' => 'Administrador Compured',
+            'correo'          => 'admin@compured.com',
+            'password'        => Hash::make('tu_contraseña_aqui'), // Pon aquí la contraseña que quieras usar
+            'rol'             => 'admin',
+            'preferencia_tema'=> 'light',
+            'fecha_registro'  => now(),
         ]);
     }
 }
